e8d6cd8b00e073b232e818561a00f5eb39da47c3
Miles <Miles-fitton@live.nmit.ac.nz>
2021-03-15 12:49:08 +1300

    workd on weefour.js

---
+++ SRC/weekfour.js
@@ -0,0 +1,17 @@
+//declared variable "country" assigned a string value of "New Zealand"
+let country = "New Zealand"
+let city = "Nelson"
+let postalCode = 7010.56
+
+console.log (typeof country)
+console.log (city)
+console.log (typeof postalCode)
+
+let isIsland = true
+let language 
+
+console.log (typeof isIsland)
+console.log (typeof language)
+
+const var = 9
+
